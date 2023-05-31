184b0fcce64ef12da06767c0e1f0fb13ec4db236
Maryam Safavi <maryam.safavi1961@gmail.com>
2023-05-31 20:56:35 +0330

    Saved PlantList Serializer added.

--- plants/serializers.py
+++ plants/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
-from .models import Plant
+
+from accounts.models import NormalUser
+from .models import Plant, SavedPlantList
 
 
 class PlantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plant
+        fields = '__all__'
+
+
+class SavedPlantListSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=NormalUser.objects.all())
+    plants = PlantSerializer(many=True)
+
+    class Meta:
+        model = SavedPlantList
         fields = '__all__'
