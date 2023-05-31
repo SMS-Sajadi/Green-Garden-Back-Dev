6a8ce2ae1f8c2c3881030ad08d388fe555ec3c63
Maryam Safavi <maryam.safavi1961@gmail.com>
2023-05-31 20:55:38 +0330

    saved plant list model added.

--- plants/models.py
+++ plants/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from accounts.models import NormalUser
 
 # Create your models here.
 class Plant(models.Model):
@@ -64,6 +64,7 @@ class Plant(models.Model):
     special_condition = models.TextField(null=True, blank=True)
     wikipedia_link = models.URLField(null=True, blank=True)
     main_img = models.ImageField(upload_to='static/plants/main_images/', default=None, null=True, blank=True)
+    # savdplantlist_user =
 
     def __str__(self):
         return self.name
@@ -74,3 +75,6 @@ class PlantImage(models.Model):
     plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
 
 
+class SavedPlantList(models.Model):
+    user = models.ForeignKey(NormalUser, on_delete=models.CASCADE)
+    plants = models.ManyToManyField(Plant)
